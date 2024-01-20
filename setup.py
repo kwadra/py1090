6368b97e6f5ebb044031c08d4f42302116dea8bc
kwadra <kwadra@users.noreply.github.com>
2024-01-20 13:53:38 -0700

    Update setup.py
    Add me as maintainer

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='py1090',
-      version='1.0a1',
+      version='1.1',
       description='ADS-B messages from BaseStation handled in a pythonic way.',
       author='Jonas Lieb',
       author_email='jonas@jonaslieb.com',
+      maintainer='kwadra',
+      maintainer_email='py1090@demosoft.org',
       url='http://www.jonaslieb.com/',
       packages=['py1090'],
       requires=[],
@@ -18,5 +20,6 @@ setup(name='py1090',
       	'License :: OSI Approved :: Apache Software License',
       	'Programming Language :: Python :: 3',
       	'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
-      ]
+      ],
+      python_requires='>=3.6',
 	)
